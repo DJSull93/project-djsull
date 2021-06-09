af7719b0f2a64a7fdf4317c35739d0a8cd541a30
DJSull93 <digi809@naver.com>
2021-06-09 17:33:02 +0900

    0609

---
+++ todos/src/App.js
@@ -0,0 +1,12 @@
+import { Schedule } from './containers'
+import { Route } from "react-router-dom"
+
+const App = () => {
+  return (
+    <>
+      <Route exact path='/' component={ Schedule }/>
+    </>
+  )
+}
+
+export default App;
