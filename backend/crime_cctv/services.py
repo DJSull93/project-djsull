382f56f21a123bebaae01f893ed91adf41dbd806
DJSull93 <digi809@naver.com>
2021-06-11 10:31:12 +0900

    0611

--- backend/crime_cctv/services.py
+++ backend/crime_cctv/services.py
@@ -19,7 +19,7 @@ class CrimecctvService(ReaderBase):
         reader = Reader()
         file.context = payload.get('context')
         file.fname = payload.get('fname')
-        printer.dframe(reader.xls(file, header=0, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
+        printer.dframe(reader.xls(file, header=1, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
 
     def json(self, payload):
         pass
